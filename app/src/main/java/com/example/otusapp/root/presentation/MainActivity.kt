59910f6a2b0adfbe06c6743b45ef89008c7bd156
leonid.belyakov <leonid.belyakov@e-legion.com>
2021-01-17 22:21:44 +0400

    Начал разделение по пакетам Создал модели для слоев Domain, Data, Presentation

--- app/src/main/java/com/example/otusapp/root/presentation/MainActivity.kt
+++ app/src/main/java/com/example/otusapp/root/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.otusapp
+package com.example.otusapp.root.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.otusapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
